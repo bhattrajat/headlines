94cfb07f82d3b7545b369637da5c0467f0a1c845
Rajat Bhatt <rajatbhatt999@gmail.com>
2024-02-01 18:11:00 -0500

    chore: add robots.txt

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,6 +37,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next) & Favicons
-    "/((?!api|_next/static|_next/image|favicon.ico|apple-icon|sitemap.xml|opengraph-image).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|apple-icon|sitemap.xml|opengraph-image|robots.txt).*)",
   ],
 };
