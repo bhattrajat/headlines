eb2e7c803f89d7b33e9fcf1785e786f80fe26f8b
Rajat Bhatt <rajatbhatt999@gmail.com>
2024-02-02 19:17:29 -0500

    fix: Remove text from skeleton

--- src/components/skeletons/news-headlines.tsx
+++ src/components/skeletons/news-headlines.tsx
@@ -3,7 +3,6 @@ import HeadlineCardSkeleton from "@/components/skeletons/headline-card";
 export default function NewsHeadlinesSkeleton() {
   return (
     <>
-      <h1 className="text-2xl">Today&apos;s headlines</h1>
       <section className="grid gap-4 lg:grid-cols-3 lg:p-10">
         {Array(9)
           .fill(0)
