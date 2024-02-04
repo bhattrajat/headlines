664681cba30259cb33031dae707bbeecde308353
Rajat Bhatt <rajatbhatt999@gmail.com>
2024-02-04 14:26:17 -0500

    fix: Better skeleton

--- src/components/skeletons/headline-card.tsx
+++ src/components/skeletons/headline-card.tsx
@@ -1,5 +1,5 @@
+import { Skeleton } from "@/components/skeletons";
+
 export default function HeadlineCardSkeleton() {
-  return (
-    <div className="h-[250px] w-[calc(100vw-1rem)] animate-pulse bg-gray-600 lg:w-[450px]"></div>
-  );
+  return <Skeleton className="h-[250px] w-[calc(100vw-1rem)] lg:w-[450px]" />;
 }
--- src/components/skeletons/news-headlines.tsx
+++ src/components/skeletons/news-headlines.tsx
@@ -1,8 +1,16 @@
+import { Skeleton } from "@/components/skeletons";
 import HeadlineCardSkeleton from "@/components/skeletons/headline-card";
+import { CATEGORIES } from "@/constants";
 
 export default function NewsHeadlinesSkeleton() {
   return (
     <>
+      <Skeleton className="mb-4 mt-2 h-6 w-[176px]" />
+      <div className="my-2 flex flex-wrap items-center justify-center gap-4">
+        {CATEGORIES.map((category) => (
+          <Skeleton key={category} className="h-8 w-20" />
+        ))}
+      </div>
       <section className="grid gap-4 lg:grid-cols-3 lg:p-10">
         {Array(9)
           .fill(0)
